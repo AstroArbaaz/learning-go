76f71c17b47dc0442bbd491b53fd04a0bffe37d0
Arbaaz <mr.khan4231@gmail.com>
2022-04-03 08:35:00 +0530

    testing

--- hello.go
+++ hello.go
@@ -1,7 +1,26 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("Hello, world")
+// }
+
+// How It Works:
+// When you write a program in Go you will have a main package defined with a main func inside it. Packages are ways of grouping up related Go code together.
+// The func keyword is how you define a function with a name and a body.
+// With import "fmt" we are importing a package which contains the Println function that we use to print.
+
+// seperating domain code with outside world;
+
 package main
 
 import "fmt"
 
+func Hello() string {
+	return "Hello, World"
+}
+
 func main() {
-	fmt.Println("Hello, world")
+	fmt.Println(Hello())
 }
